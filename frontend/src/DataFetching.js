6889f2184fdbe037754e618e6db4a2741622534f
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-02-12 13:30:40 +0000

    added styling to buttons

--- frontend/src/DataFetching.js
+++ frontend/src/DataFetching.js
@@ -14,7 +14,7 @@ function Product(props) {
 
     return (
      <div>
-    <div style={{marginLeft: "40px",backgroundColor: "white", border: "none", width: "600px", height: "30%", padding: "10px", display: "inline-block", border: "1px solid white"}}>
+    <div style={{marginLeft: "40px",backgroundColor: "white", width: "600px", height: "30%", padding: "10px", display: "inline-block", border: "1px solid white"}}>
       <p style={{border: "1px solid rgb(50 43 128)",  padding: "5px", display: "inline-block", color: "white",  backgroundColor: "rgb(50 43 128)",}}>{props.product.category}</p>
       <h2 style={{ fontWeight: "bold", fontSize: "30px", marginTop: 0, marginBottom: '10px', }}>{props.product.title}</h2>
       <p style={{marginTop: 0, marginBottom: '10px', }}>{props.product.description}</p>
@@ -100,20 +100,20 @@ function Datafetching() {
             <div className="dropdown">
                 <select style= {{ marginLeft: "80px", width: "200px", height: "40px",fontSize: "20px", alignContent: "center", border: "none" }}className="dropdown-content"value={sortBy} onChange={handleSort}>
                 <option value="category">Sort by Category</option>
-                <option value="title">Sort by Title</option>
-                <option value="description">Sort by Description</option>
-                <option value="price">Sort by Price</option>
-                <option value="stock">Sort by Stock</option>
+                    <option value="title">Sort by Title</option>
+                    <option value="description">Sort by Description</option>
+                    <option value="price">Sort by Price</option>
+                    <option value="stock">Sort by Stock</option>
                 </select>
             </div>
             <div className="buttons">
-                <button style={{backgroundColor:"rgb(50 43 128", color: "white", fontSize: "19px", width: "130px", height: "33px",margin: "5px", border: "none"}} onClick={() => handleFilter("products")}>All products</button> 
-                <button style={{backgroundColor:"rgb(160 191 56",color: "white",fontSize: "19px", width: "100px", height: "33px",margin: "5px", margin: "5px", border: "none"}} onClick={() => handleFilter("groceries")}>Groceries</button>
-                <button style={{backgroundColor:"rgb(223 153 27",color: "white", fontSize: "19px", width: "160px", height: "33px",margin: "5px", whiteSpace: "nowrap", margin: "5px", border: "none"}} onClick={() => handleFilter("home-decoration")}>Home Decoration</button>
-                <button style={{backgroundColor:"rgb(222 160 195",color: "white", fontSize: "19px", width: "100px", height: "33px",margin: "5px", margin: "5px", border: "none"}} onClick={() => handleFilter("fragrances")}>Fragrances</button>
-                <button style={{backgroundColor:"rgb(244 33 138",color: "white", fontSize: "19px", width: "100px", height: "33px",margin: "5px", margin: "5px", border: "none"}} onClick={() => handleFilter("skincare")}>Skincare</button>
-                <button style={{backgroundColor:"rgb(127 127 127",color: "white", fontSize: "19px", width: "100px", height: "33px",margin: "5px", margin: "5px", border: "none"}} onClick={() => handleFilter("laptops")}>Laptops</button>
-                <button style={{backgroundColor:"rgb(154 206 243",color: "white", fontSize: "19px", width: "130px", height: "33px",margin: "5px", margin: "5px", border: "none"}} onClick={() => handleFilter("smartphones")}>Smartphones</button>
+                <button style={{backgroundColor:"rgb(50 43 128", color: "white", fontSize: "19px", width: "130px", height: "33px",margin: "5px", margin_left: "5px", border: "none"}} onClick={() => handleFilter("products")}>All products</button> 
+                <button style={{backgroundColor:"rgb(160 191 56",color: "white",fontSize: "19px", width: "100px", height: "33px",margin: "5px", border: "none"}} onClick={() => handleFilter("groceries")}>Groceries</button>
+                <button style={{backgroundColor:"rgb(223 153 27",color: "white", fontSize: "19px", width: "160px", height: "33px",margin: "5px", whiteSpace: "nowrap",  border: "none"}} onClick={() => handleFilter("home-decoration")}>Home Decoration</button>
+                <button style={{backgroundColor:"rgb(222 160 195",color: "white", fontSize: "19px", width: "100px", height: "33px",margin: "5px", border: "none"}} onClick={() => handleFilter("fragrances")}>Fragrances</button>
+                <button style={{backgroundColor:"rgb(244 33 138",color: "white", fontSize: "19px", width: "100px", height: "33px",margin: "5px", border: "none"}} onClick={() => handleFilter("skincare")}>Skincare</button>
+                <button style={{backgroundColor:"rgb(127 127 127",color: "white", fontSize: "19px", width: "100px", height: "33px",margin: "5px", border: "none"}} onClick={() => handleFilter("laptops")}>Laptops</button>
+                <button style={{backgroundColor:"rgb(154 206 243",color: "white", fontSize: "19px", width: "130px", height: "33px",margin: "5px", border: "none"}} onClick={() => handleFilter("smartphones")}>Smartphones</button>
             </div>
            
             <div classname="items-container">
